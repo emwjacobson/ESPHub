2916b4486498473ad0b6a6f171d861c03a6100e1
Emerson Jacobson <emwjacobson@gmail.com>
2021-07-25 22:01:49 -0700

    Started adding web server to master

--- src/master.cpp
+++ src/master.cpp
@@ -5,18 +5,44 @@
 #include <ESP8266WiFi.h>
 #include "master.h"
 #include "config.h"
+// #include "ESPAsyncTCP.h"
+// #include "ESPAsyncWebServer.h"
+
+Master::Master()/*: server(AsyncWebServer(80))*/ {}
 
 void Master::setup() {
   WiFi.mode(WIFI_AP_STA);
   this->setupSoftAP();
   this->connectToAP();
+  // this->registerEndpoints();
 }
 
 void Master::loop() {
   #ifdef SERIAL_ENABLED
   Serial.print("Num connected: "); Serial.println(WiFi.softAPgetStationNum());
-  Serial.print("WIFI Status: "); Serial.println(WiFi.status());
+  Serial.print("WIFI Status: ");
+  switch(WiFi.status()) {
+    case WL_CONNECTED:
+      Serial.println("Connected");
+      break;
+    case WL_NO_SSID_AVAIL:
+      Serial.println("SSID Not Found");
+      break;
+    case WL_CONNECT_FAILED:
+      Serial.println("Connection Failed");
+      break;
+    case WL_WRONG_PASSWORD:
+      Serial.println("Wrong Password");
+      break;
+    case WL_DISCONNECTED:
+      Serial.println("Disconnected");
+      break;
+    default:
+      Serial.println(WiFi.status());
+  }
+  Serial.println("");
   #endif
+
   delay(1000);
 }
 
@@ -33,5 +59,11 @@ void Master::setupSoftAP() {
 void Master::connectToAP() {
   WiFi.begin(EXTERNAL_SSID, EXTERNAL_PASSWD);
 }
+
+// void Master::registerEndpoints() {
+//   this->server.on("/", HTTP_POST, [](AsyncWebServerRequest* request) {
+//     Serial.println("Got Request!");
+//   });
+// }
 
 #endif
--- src/master.h
+++ src/master.h
@@ -3,16 +3,22 @@
 
 #include <Arduino.h>
 #include "controller.h"
+#include "ESPAsyncWebServer.h"
 
 class Master : public Controller {
 public:
+  Master();
   void setup();
   void loop();
 private:
+  // AsyncWebServer server;
+
   // Creates a soft AP
   void setupSoftAP();
   // Connects to an AP
   void connectToAP();
+  // Register web server endpoints
+  void registerEndpoints();
 };
 
 #endif

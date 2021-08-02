1997ab2f0fb11998a88ff9a5271f9c2c55beeb10
Emerson Jacobson <emwjacobson@gmail.com>
2021-08-01 23:05:23 -0700

    Added sensor array to follower class

--- include/follower.h
+++ include/follower.h
@@ -1,13 +1,17 @@
 #ifndef FOLLOWER_H
 #define FOLLOWER_D
 
+#include <Array.h>
 #include "controller.h"
+#include "sensors/sensor.h"
+#include "config.h"
 
 class Follower : public Controller {
 public:
   Follower();
   void loop() override;
 private:
+  Array<Sensor, MAX_SENSORS> sensors;
 };
 
 #endif
